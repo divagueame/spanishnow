18b5ee57659ac744e284b10855042b33bc0cb4b6
Divagueame <martin.arce.teixeira@gmail.com>
2022-08-04 19:45:42 +0200

    End

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -3,37 +3,35 @@ class WebhooksController < ApplicationController
     puts "\nCHECKOUT"
     puts "\n    "
     puts "\n    "
-    puts "\n    " 
-    
+    puts "\n    "
+
     payload = request.body.read
-  event = nil
+    event = nil
 
-  begin
-    event = Stripe::Event.construct_from(
-      JSON.parse(payload, symbolize_names: true)
-    )
-  rescue JSON::ParserError => e
-    # Invalid payload
-    puts "⚠️  Webhook error while parsing basic request. #{e.message})"
-    status 400
-    return
-end
+    begin
+      event = Stripe::Event.construct_from(
+        JSON.parse(payload, symbolize_names: true)
+      )
+    rescue JSON::ParserError => e
+      # Invalid payload
+      puts "⚠️  Webhook error while parsing basic request. #{e.message})"
+      status 400
+      return
+    end
 
-# Handle the event
-case event.type
-    
-when 'checkout_session_completed'
-    puts "\n COMPLETED!"
-    puts "\n    "
-    puts "\n    "
-    puts "\n    " 
+    # Handle the event
+    case event.type
 
-  else
-    puts "Unhandled event type: #{event.type}"
+    when 'checkout_session_completed'
+      puts "\n COMPLETED!"
+      puts "\n    "
+      puts "\n    "
+      puts "\n    "
+
+    else
+      puts "Unhandled event type: #{event.type}"
+    end
+
+    status 200
   end
-
-  status 200
-  
- 
-
 end
