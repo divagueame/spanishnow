fabdddc7c9e1b2368026f162ff987651394b6439
Divagueame <martin.arce.teixeira@gmail.com>
2022-08-04 19:44:10 +0200

    Retry

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,55 +1,39 @@
 class WebhooksController < ApplicationController
   def create
+    puts "\nCHECKOUT"
+    puts "\n    "
+    puts "\n    "
+    puts "\n    " 
+    
     payload = request.body.read
-    sig_header = request.env['HTTP_STRIPE_SIGNATURE']
-    event = nil
-    p 'GOT'
-    puts "\nTO SATRT"
-    puts "\nTO SATRT"
-    puts "\nTO SATRT"
-    puts "\nTO SATRT"
-    puts "\n "
-    puts "\n "
-    puts "\n "
-    begin
-      event = Stripe::Webhook.construct_event(
-        payload, sig_header, Rails.application.credentials[:stripe][:webhook]
-      )
-    rescue JSON::ParserError => e
-      # Invalid payload
-      puts "\nJSON PARSER ERROR"
-      puts "\nJSON PARSER ERROR"
-      puts "\nJSON PARSER ERROR"
-      status 400
-      return
-    end
+  event = nil
 
-    # Handle the event
-    case event.type
-    when 'checkout_session_completed'
-      puts "\nJCHECKOUT"
-      puts "\n    "
-      puts "\n    "
-      puts "\n    "
-      puts "\n    "
-      puts "\n"
-      session = event.data.object
-      @product = Product.find_by(price: session.amount_total)
-      puts "\n PRODUCT"
-      puts @product
-      @product.increment(:sales_count)
-    when 'payment_intent.succeeded'
-      payment_intent = event.data.object # contains a Stripe::PaymentIntent
-      puts 'PaymentIntent was successful!'
-    when 'payment_method.attached'
-      payment_method = event.data.object # contains a Stripe::PaymentMethod
-      puts 'PaymentMethod was attached to a Customer!'
-    # ... handle other event types
-    else
-      puts "Unhandled event type: #{event.type}"
-    end
+  begin
+    event = Stripe::Event.construct_from(
+      JSON.parse(payload, symbolize_names: true)
+    )
+  rescue JSON::ParserError => e
+    # Invalid payload
+    puts "⚠️  Webhook error while parsing basic request. #{e.message})"
+    status 400
+    return
+end
 
-    status 200
-    render json: { message: success }
+# Handle the event
+case event.type
+    
+when 'checkout_session_completed'
+    puts "\n COMPLETED!"
+    puts "\n    "
+    puts "\n    "
+    puts "\n    " 
+
+  else
+    puts "Unhandled event type: #{event.type}"
   end
+
+  status 200
+  
+ 
+
 end
