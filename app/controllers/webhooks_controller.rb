3effde497663649321de78d98879056a063ebd39
Divagueame <martin.arce.teixeira@gmail.com>
2022-08-04 19:28:47 +0200

    Bug

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,42 +1,37 @@
 class WebhooksController < ApplicationController
   def create
-  payload = request.body.read
-  sig_header = request.env['HTTP_STRIPE_SIGNATURE']
-  event = nil
+    payload = request.body.read
+    sig_header = request.env['HTTP_STRIPE_SIGNATURE']
+    event = nil
 
-  begin 
-    event = Stripe::Webhook.construct_event(
+    begin
+      event = Stripe::Webhook.construct_event(
         payload, sig_header, Rails.application.credentials[:stripe][:webhook]
-    )
-  rescue JSON::ParserError => e
-    # Invalid payload
-    status 400
-    return
-  end
+      )
+    rescue JSON::ParserError => e
+      # Invalid payload
+      status 400
+      return
+    end
 
-  # Handle the event
-  case event.type
-  when 'checkout_session_completed'
-    session = event.data.object
-    @product = Product.find_by(price: session.amount_total)
-    @product.increment(:sales_count)
-  when 'payment_intent.succeeded'
-    payment_intent = event.data.object # contains a Stripe::PaymentIntent
-    puts 'PaymentIntent was successful!'
-  when 'payment_method.attached'
-    payment_method = event.data.object # contains a Stripe::PaymentMethod
-    puts 'PaymentMethod was attached to a Customer!'
-  # ... handle other event types
-  else
-    puts "Unhandled event type: #{event.type}"
-  end
+    # Handle the event
+    case event.type
+    when 'checkout_session_completed'
+      session = event.data.object
+      @product = Product.find_by(price: session.amount_total)
+      @product.increment(:sales_count)
+    when 'payment_intent.succeeded'
+      payment_intent = event.data.object # contains a Stripe::PaymentIntent
+      puts 'PaymentIntent was successful!'
+    when 'payment_method.attached'
+      payment_method = event.data.object # contains a Stripe::PaymentMethod
+      puts 'PaymentMethod was attached to a Customer!'
+    # ... handle other event types
+    else
+      puts "Unhandled event type: #{event.type}"
+    end
 
-  status 200
-  render json: {message: success}
-end
-
-
-
-
+    status 200
+    render json: { message: success }
   end
 end
