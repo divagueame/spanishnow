f48c6274887cf00ab8a69da0cc9df75d1e86a228
Divagueame <martin.arce.teixeira@gmail.com>
2022-08-12 06:51:51 +0200

    Add user registration after payment

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -13,6 +13,7 @@ class WebhooksController < ApplicationController
       event = Stripe::Webhook.construct_event(
         payload, sig_header, Rails.application.credentials[:stripe][:webhook]
       )
+      
     rescue JSON::ParserError => e
       status 400
       return
@@ -23,16 +24,25 @@ class WebhooksController < ApplicationController
       return
     end
 
+    puts event.type
     case event.type
+    when 'checkout.session.completed'
+      puts "ppppppppppppppp1\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nppppppppppppPPPPPP"
+      user_email = event.data.object.customer_details.email
+      user_name = event.data.object.customer_details.name
+      payment_status = event.data.object.payment_status
+
+      generated_password = Devise.friendly_token.first(8)
+      user = User.create!(:email => user_email, :password => generated_password)
+
+
+
+      puts "ppppppppppppppp1\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nppppppppppppPPPPPP"
     when 'payment_intent.succeeded'
-
-      puts event.type
-      # Handle the event
-      puts "\n\n\n\n---"
+      
       Product.last.increment!(:sales_count) 
- 
     end
-
+    # status 200
     render json: { message: 'success' }
   end
 
