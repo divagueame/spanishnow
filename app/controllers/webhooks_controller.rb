fef3ff31368884acbbe82f705c018e5292f1b190
Divagueame <martin.arce.teixeira@gmail.com>
2022-08-13 22:40:07 +0200

    Adding Flash

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -27,19 +27,13 @@ class WebhooksController < ApplicationController
     puts event.type
     case event.type
     when 'checkout.session.completed'
-      puts "ppppppppppppppp1\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nppppppppppppPPPPPP"
       user_email = event.data.object.customer_details.email
       user_name = event.data.object.customer_details.name
       payment_status = event.data.object.payment_status
 
       generated_password = Devise.friendly_token.first(8)
       user = User.create!(:email => user_email, :password => generated_password)
-
-
-
-      puts "ppppppppppppppp1\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nppppppppppppPPPPPP"
     when 'payment_intent.succeeded'
-      
       Product.last.increment!(:sales_count) 
     end
     # status 200
