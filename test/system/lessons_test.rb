357dbb1c9fbf62f69d91a435b72addd18f787e69
Divagueame <martin.arce.teixeira@gmail.com>
2022-08-30 23:58:54 +0200

    Add tests for lessons

--- test/system/lessons_test.rb
+++ test/system/lessons_test.rb
@@ -14,38 +14,49 @@ class LessonsTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Lessons"
   end
 
-  # test "should create lesson" do
-  #   visit lessons_url
-  #   click_on "New lesson"
+  test "should create lesson" do
+    sign_in(@admin_user)
+    visit lessons_url
+    click_on "New lesson"
 
-  #   fill_in "Description", with: @lesson.description
-  #   fill_in "Position", with: @lesson.position
-  #   fill_in "Product", with: @lesson.product_id
-  #   fill_in "Title", with: @lesson.title
-  #   click_on "Create Lesson"
+    fill_in "Description", with: @lesson.description
+    fill_in "Position", with: @lesson.position
+    fill_in "Product", with: @lesson.product_id
+    fill_in "Title", with: @lesson.title
+    click_on "Create Lesson"
 
-  #   assert_text "Lesson was successfully created"
-  #   click_on "Back"
-  # end
+    assert_text "Lesson was successfully created"
+    click_on "Back"
+  end
 
-  # test "should update Lesson" do
-  #   visit lesson_url(@lesson)
-  #   click_on "Edit this lesson", match: :first
+  test "should update Lesson" do
+    sign_in(@admin_user)
+    visit lesson_url(@lesson)
+    click_on "Edit this lesson", match: :first
 
-  #   fill_in "Description", with: @lesson.description
-  #   fill_in "Position", with: @lesson.position
-  #   fill_in "Product", with: @lesson.product_id
-  #   fill_in "Title", with: @lesson.title
-  #   click_on "Update Lesson"
+    fill_in "Description", with: @lesson.description
+    fill_in "Position", with: @lesson.position
+    fill_in "Product", with: @lesson.product_id
+    fill_in "Title", with: @lesson.title
+    click_on "Update Lesson"
 
-  #   assert_text "Lesson was successfully updated"
-  #   click_on "Back"
-  # end
+    assert_text "Lesson was successfully updated"
+    click_on "Back"
+  end
 
-  # test "should destroy Lesson" do
-  #   visit lesson_url(@lesson)
-  #   click_on "Destroy this lesson", match: :first
 
-  #   assert_text "Lesson was successfully destroyed"
-  # end
+  test "Logged in users should not update or destroy lesson" do
+    sign_in(@logged_in_user)
+    visit lesson_url(@lesson)
+    assert_no_text "Edit this lesson"
+    assert_no_text "Destroy this lesson"
+  end
+
+  test "should destroy Lesson" do
+    sign_in(@admin_user)
+    visit lesson_url(@lesson)
+    click_on "Destroy this lesson", match: :first
+
+    assert_text "Lesson was successfully destroyed"
+  end
 end
