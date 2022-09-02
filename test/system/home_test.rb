40806ec94546ce074c6d53b66ebf6e1077c1c844
Divagueame <martin.arce.teixeira@gmail.com>
2022-09-02 22:34:26 +0200

    Add footer template

--- test/system/home_test.rb
+++ test/system/home_test.rb
@@ -4,12 +4,12 @@ class HomeTest < ApplicationSystemTestCase
 
   test "visiting the index" do
     visit root_url
-    assert_selector "h1", text: "Mambo Spanish"
+    assert_selector "img", class: "logo"
   end
 
   test "buying the product" do
     visit root_url
-    assert_selector "h1", text: "Mambo Spanish"
+    assert_selector "img", class: "logo"
     # assert_selector "button", text: "Buy now and start learning today!"
     # click_button
   end
