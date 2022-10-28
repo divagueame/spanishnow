042f6f80ab7a18fd3bfc525cb4c3501356d1d628
Martin Arce <martin.arce.teixeira@gmail.com>
2022-10-28 21:53:47 +0200

    Update bundle

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,6 @@
 # Pin npm packages by running ./bin/importmap
 
+
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.1.0
