fa8001a4afaa2ccd6f3eefb04786a515ebba0602
Martin Arce <martin.arce.teixeira@gmail.com>
2022-11-01 20:23:56 +0100

    Adjust form for multiple

--- app/controllers/multiple_open_blocks_controller.rb
+++ app/controllers/multiple_open_blocks_controller.rb
@@ -1,35 +1,31 @@
 class MultipleOpenBlocksController < ApplicationController
   before_action :authenticate_admin, only: %i[index new create destroy edit update]
-  before_action :set_multiple_open_block, only: %i[ show edit update destroy ]
+  before_action :set_multiple_open_block, only: %i[show edit update destroy]
 
   def index
     @multiple_open_blocks = MultipleOpenBlock.all
   end
 
-  def show
-  end
+  def show; end
 
   def new
-    p "NEw!"
-    @lesson_group_id = params[:lesson_group_id] 
+    @lesson_group_id = params[:lesson_group_id]
     @multiple_open_block = MultipleOpenBlock.new
     @multiple_open_block.build_lesson_block
-    # @multiple_open_block.build_lesson_block(lesson_id: params[:lesson_id])
-
-    # 15.times { @multiple_open_block.multiple_open_pieces.build }
+    @multiple_open_block.multiple_open_pieces.build
   end
 
-  def edit
-  end
+  def edit; end
 
   def create
     @multiple_open_block = MultipleOpenBlock.new(multiple_open_block_params)
+
     respond_to do |format|
-      if @multiple_open_block.save
-        format.html {
-          redirect_to lessons_path
-          # redirect_to lesson_path(@multiple_open_block.lesson_group.lesson), notice: "Multiple open block was successfully created."
-        }
+      if @multiple_open_block.lesson_block.save
+        format.html do
+          lesson_group = multiple_open_block_params[:lesson_block_attributes][:lesson_group_id]
+          redirect_to lesson_group_path(lesson_group), notice: 'Multiple open block was successfully created.'
+        end
       else
         format.html { render :new, status: :unprocessable_entity }
       end
@@ -39,7 +35,10 @@ class MultipleOpenBlocksController < ApplicationController
   def update
     respond_to do |format|
       if @multiple_open_block.update(multiple_open_block_params)
-        format.html { redirect_to multiple_open_block_url(@multiple_open_block), notice: "Multiple open block was successfully updated." }
+        format.html do
+          redirect_to multiple_open_block_url(@multiple_open_block),
+                      notice: 'Multiple open block was successfully updated.'
+        end
       else
         format.html { render :edit, status: :unprocessable_entity }
       end
@@ -50,19 +49,20 @@ class MultipleOpenBlocksController < ApplicationController
     @multiple_open_block.destroy
 
     respond_to do |format|
-      format.html { redirect_to multiple_open_blocks_url, notice: "Multiple open block was successfully destroyed." }
+      format.html { redirect_to multiple_open_blocks_url, notice: 'Multiple open block was successfully destroyed.' }
     end
   end
 
   private
-    def set_multiple_open_block
-      @multiple_open_block = MultipleOpenBlock.find(params[:id])
-    end
 
-    def multiple_open_block_params
-      params.require(:multiple_open_block)
-            .permit(:title, :body,
-              multiple_open_pieces_attributes: [:id, :full, :shown, :hint],
-              lesson_block_attributes: [:title, :id, :lesson_group_id])
-    end
+  def set_multiple_open_block
+    @multiple_open_block = MultipleOpenBlock.find(params[:id])
+  end
+
+  def multiple_open_block_params
+    params.require(:multiple_open_block)
+          .permit(:title, :body,
+                  multiple_open_pieces_attributes: %i[id full shown hint],
+                  lesson_block_attributes: %i[title id lesson_group_id])
+  end
 end
--- app/models/multiple_open_block.rb
+++ app/models/multiple_open_block.rb
@@ -1,14 +1,10 @@
 class MultipleOpenBlock < ApplicationRecord
-    has_one :lesson_block, as: :block, dependent: :destroy
-    has_one :lesson_group, through: :lesson_block
-    has_many :multiple_open_pieces, dependent: :destroy
-    
-    accepts_nested_attributes_for :lesson_block
+  has_one :lesson_block, as: :block, dependent: :destroy
+  has_one :lesson_group, through: :lesson_block
+  has_many :multiple_open_pieces, dependent: :destroy
 
-    # accepts_nested_attributes_for :multiple_open_pieces,
-    #     reject_if: lambda {|attributes| attributes['full'].blank? || attributes['shown'].blank? }
+  accepts_nested_attributes_for :lesson_block
 
-
-        
-    
+  accepts_nested_attributes_for :multiple_open_pieces
+  #     reject_if: lambda {|attributes| attributes['full'].blank? || attributes['shown'].blank? }
 end
