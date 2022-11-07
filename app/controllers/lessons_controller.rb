3db19bd389591b11febb28fbbd7b156b37aae7fd
Martin Arce <martin.arce.teixeira@gmail.com>
2022-11-07 16:14:32 +0100

    Edit lesson info turbo

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,48 +1,47 @@
 class LessonsController < ApplicationController
-  before_action :authenticate_admin, only: %i[ new edit create update destroy toggle_active_lesson]
-  before_action :authenticate_user!, only: %i[ index show]
-  before_action :set_lesson, only: %i[ show edit update destroy toggle_active_lesson]
+  before_action :authenticate_admin, only: %i[new edit create update destroy toggle_active_lesson]
+  before_action :authenticate_user!, only: %i[index show]
+  before_action :set_lesson, only: %i[show edit update destroy toggle_active_lesson]
 
-  before_action only: %i[ show ] do
+  before_action only: %i[show] do
     filter_active(@lesson)
   end
 
-  before_action only: %i[ show ] do
+  before_action only: %i[show] do
     filter_study_session(@lesson) unless current_user.admin?
   end
 
   def index
-    if current_user.admin?
-      @lessons = Lesson.all
-    else
-      @lessons = Lesson.where(active: true)
-    end
+    @lessons = if current_user.admin?
+                 Lesson.all
+               else
+                 Lesson.where(active: true)
+               end
 
     @lonely_lesson_groups = LessonGroup.where(lesson_id: nil)
   end
 
   def show
     @lesson_groups = @lesson.lesson_groups
-    if !current_user.admin?
-      @study_session = current_user.study_session
-      @active_lesson_group = LessonGroup.find(@study_session.lesson_group_id)
-    end
+    return if current_user.admin?
+
+    @study_session = current_user.study_session
+    @active_lesson_group = LessonGroup.find(@study_session.lesson_group_id)
   end
 
   def new
     @lesson = Lesson.new
   end
 
-  def edit
-  end
+  def edit; end
 
   def create
     @lesson = Lesson.new(lesson_params)
 
     respond_to do |format|
       if @lesson.save
-        format.html { redirect_to lesson_url(@lesson), notice: "Lesson was successfully created." }
-      else 
+        format.html { redirect_to lesson_url(@lesson), notice: 'Lesson was successfully created.' }
+      else
         format.html { render :new, status: :unprocessable_entity }
       end
     end
@@ -51,7 +50,8 @@ class LessonsController < ApplicationController
   def update
     respond_to do |format|
       if @lesson.update(lesson_params)
-        format.html { redirect_to lesson_url(@lesson), notice: "Lesson was successfully updated." }
+        format.html { redirect_to lesson_url(@lesson), notice: 'Lesson was successfully updated.' }
+        format.turbo_stream { flash.now[:notice] = "Lesson was successfully updated." }
       else
         format.html { render :edit, status: :unprocessable_entity }
       end
@@ -62,7 +62,7 @@ class LessonsController < ApplicationController
     @lesson.destroy
 
     respond_to do |format|
-      format.html { redirect_to lessons_url, notice: "Lesson was successfully destroyed.", status: 303 }
+      format.html { redirect_to lessons_url, notice: 'Lesson was successfully destroyed.', status: 303 }
     end
   end
 
@@ -72,11 +72,12 @@ class LessonsController < ApplicationController
   end
 
   private
-    def set_lesson
-      @lesson = Lesson.find(params[:id])
-    end
 
-    def lesson_params
-      params.require(:lesson).permit(:title, :description, :position, :product_id, :active)
-    end
+  def set_lesson
+    @lesson = Lesson.find(params[:id])
+  end
+
+  def lesson_params
+    params.require(:lesson).permit(:title, :description, :position, :product_id, :active)
+  end
 end
--- app/helpers/lessons_helper.rb
+++ app/helpers/lessons_helper.rb
@@ -2,13 +2,14 @@ module LessonsHelper
   def controls_button(direction, lesson_group)
     disabled = false
     if  direction == 'Next' && lesson_group.last? ||
-        direction == 'Previous' && lesson_group.first?
+      direction == 'Previous' && lesson_group.first? ||
+      direction == 'Finish' && !lesson_group.last?
       disabled = true
       return
     end
-
+    
     button_to direction,
-              { controller: 'study_session', action: 'update', direction: },
+              { controller: 'study_session', action: 'update', direction: direction},
               method: :patch,
               disabled: disabled,
               class: "px-4 py-2 hover:bg-green-100 #{disabled ? 'bg-red-200' : 'bg-purple-100'}"
