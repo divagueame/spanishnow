a5e78f17bc00e1e4a43af5a5c7d731490a625c23
Martin Arce <martin.arce.teixeira@gmail.com>
2022-11-14 20:56:03 +0100

    Update session controllers

--- app/helpers/lessons_helper.rb
+++ app/helpers/lessons_helper.rb
@@ -1,17 +1,24 @@
 module LessonsHelper
   def controls_button(direction, lesson_group)
     disabled = false
-    if  direction == 'Next' && lesson_group.last? ||
-      direction == 'Previous' && lesson_group.first? ||
-      direction == 'Finish' && !lesson_group.last?
+    if
+      direction == 'Previous' && lesson_group.first?
       disabled = true
-      return
     end
     
+    if direction == 'Next' && lesson_group.last? then
+      button_to 'Finish',
+              { controller: 'study_session', action: 'update', direction: 'Finish'},
+              method: :patch,
+              form: { data:  { turbo_confirm: "This is the last block of this lesson. You won't be able to work on it anymore. Are you sure you are done?" }},
+              class: "px-12 py-3 bg-amber-500 text-bold hover:bg-amber-300 transition-all"
+    else
+
     button_to direction,
               { controller: 'study_session', action: 'update', direction: direction},
               method: :patch,
               disabled: disabled,
-              class: "px-12 py-4 hover:bg-green-100 #{disabled ? 'bg-red-200' : 'bg-purple-100'}"
+              class: "px-12 py-3 #{disabled ? 'bg-slate-100 text-slate-400 cursor-default opacity-80' : 'bg-orange-100 hover:bg-orange-200 transition-all'}"
+      end
   end
 end
