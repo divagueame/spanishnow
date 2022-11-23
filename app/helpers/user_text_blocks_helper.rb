6bd77d55283b9ecf47e3b4a5f81e62dee05789cf
Martin Arce <martin.arce.teixeira@gmail.com>
2022-11-23 20:28:11 +0100

    User text answer in Turbo stream

--- app/helpers/user_text_blocks_helper.rb
+++ app/helpers/user_text_blocks_helper.rb
@@ -1,13 +1,3 @@
 module UserTextBlocksHelper
-    def answers_links(user_text_block)
-        if !user_text_block.answered?(current_user)
-            link_to 'Start', new_user_text_block_user_text_answer_path(user_text_block)
-        else
-            link_to 'Review your submission', user_text_block_user_text_answer_path(user_text_block, user_text_block.user_text_answers.where(user_id: current_user.id).first)
-        end
-    end
-end
 
-#     if block.block.user_text_answers.where(user_id: current_user.id).first.feedback.present?
-#       link_to 'Check it out', feedback_path(block.block.user_text_answers.where(user_id: current_user.id).first.feedback.id)
-#     end 
+end
