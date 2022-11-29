74de6713267af1550ad6ea1a9f8027eb954a4918
Martin Arce <martin.arce.teixeira@gmail.com>
2022-11-29 20:55:38 +0100

    Preparing Multiple Pieces

--- app/models/multiple_open_piece.rb
+++ app/models/multiple_open_piece.rb
@@ -1,4 +1,8 @@
 class MultipleOpenPiece < ApplicationRecord
   belongs_to :multiple_open_block
   has_many :multiple_open_answers, dependent: :destroy
+  
+  def get_hidden_field
+   ['hidden filed','after field']
+  end
 end
