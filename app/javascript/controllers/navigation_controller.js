22978876f4cbd0e3fd24ea8ad0e542e204597e95
Martin Arce <martin.arce.teixeira@gmail.com>
2022-12-16 19:52:18 +0100

    Improve navigation bar

--- app/javascript/controllers/navigation_controller.js
+++ app/javascript/controllers/navigation_controller.js
@@ -2,7 +2,10 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="navigation"
 export default class extends Controller {
-  static targets = [ "toggler", "panel" ]
+  static targets = [ "toggler", "panel", "active" ]
+	connect () {
+	  this.activeTarget.scrollIntoView()
+	}
   toggle () {
 	this.element.classList.toggle("ml-96")
   	this.panelTarget.classList.toggle("-translate-x-full")
