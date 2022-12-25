1c00bfc2a1dc94e91e4caf87c54182a87ab548ff
Martin Arce <martin.arce.teixeira@gmail.com>
2022-12-25 21:25:18 +0100

    Notifications

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -5,6 +5,10 @@ include ActionView::RecordIdentifier
   belongs_to :user_text_answer
   has_one :user, through: :user_text_answer
   has_one :notification, as: :notifeable, dependent: :destroy
+  
+  def get_lesson_group
+     self.user_text_answer.user_text_block.lesson_group
+  end
 
   after_create_commit -> {
     broadcast_update_to dom_id(self.user, 'unseen_feedback'),
